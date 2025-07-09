d380c4a58dc8d0963edbb325a340a6fa77267588
Nicolas Izard <tickrace.contact@gmail.com>
2025-07-09 16:14:21 +0200

    Utilisation des variables d’environnement Vercel

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pecotcxpcqfkwvyylvjv.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlY290Y3hwY3Fma3d2eXlsdmp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwNjc1NDQsImV4cCI6MjA2NzY0MzU0NH0.DYbgJHjSuyJ-a6Iy_2w8uhUcXu_p9k6BZV0_DGyIG04";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
