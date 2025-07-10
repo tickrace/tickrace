5533a37884417b44770444d6f855f7e213ae4724
Nicolas Izard <tickrace.contact@gmail.com>
2025-07-10 15:14:08 +0200

    Correction App.jsx avec toutes les routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import SignupOrganisateur from "./pages/SignupOrganisateur";
-
-<Route path="/signup-organisateur" element={<SignupOrganisateur />} />
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Admin from "./pages/Admin";
-import Coureur from "./pages/Coureur";
-import Organisateur from "./pages/Organisateur";
-import Courses from "./pages/Courses";
-import NewCourse from "./pages/NewCourse";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import Organisateur from "./pages/Organisateur";
+import SignupOrganisateur from "./pages/SignupOrganisateur";
+import EspaceOrganisateur from "./pages/EspaceOrganisateur";
+import NouvelleCourse from "./pages/NouvelleCourse";
+import Coureur from "./pages/Coureur";
+import Admin from "./pages/Admin";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/coureur" element={<Coureur />} />
-        <Route path="/organisateur" element={<Organisateur />} />
         <Route path="/courses" element={<Courses />} />
-        <Route path="/organisateur/nouvelle-course" element={<NewCourse />} />
+        <Route path="/organisateur" element={<Organisateur />} />
+        <Route path="/organisateur/signup" element={<SignupOrganisateur />} />
+        <Route path="/organisateur/espace" element={<EspaceOrganisateur />} />
+        <Route path="/organisateur/nouvelle-course" element={<NouvelleCourse />} />
+        <Route path="/coureur" element={<Coureur />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
-git config --global user.name "Nicolas Izard"
-git config --global user.email "tickrace.contact@gmail.com"
-
