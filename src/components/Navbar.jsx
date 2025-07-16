92b378248e65f689c733b1e244db2e02d102bbe7
Nicolas Izard <tickrace.contact@gmail.com>
2025-07-16 09:07:47 +0200

    Mise à jour de Navbar avec lien vers la page Formats

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 text-white p-4 flex gap-4">
+    <nav className="bg-gray-800 text-white p-4 flex flex-wrap gap-4">
       <Link to="/" className="hover:underline">Accueil</Link>
       <Link to="/courses" className="hover:underline">Épreuves</Link>
-      <Link to="/organisateur" className="hover:underline">+ Nouvelle course</Link>
-<Link to="/organisateur/login" className="hover:underline">Connexion</Link>
-<Link to="/organisateur/espace" className="hover:underline">Mon espace</Link>
-<Link to="/organisateur/signup" className="hover:underline">Créer un compte</Link>
-
-
+      <Link to="/organisateur/nouvelle-course" className="hover:underline">+ Nouvelle course</Link>
+      <Link to="/organisateur/espace" className="hover:underline">Espace Organisateur</Link>
+      <Link to="/organisateur/login" className="hover:underline">Connexion</Link>
+      <Link to="/organisateur/signup" className="hover:underline">Créer un compte</Link>
+      <Link to="/formats" className="hover:underline">Formats</Link>
+      <Link to="/coureur" className="hover:underline">Coureur</Link>
+      <Link to="/admin" className="hover:underline">Admin</Link>
     </nav>
   );
 }
