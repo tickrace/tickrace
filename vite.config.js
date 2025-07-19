8727210033e4cb17b57cc2fe7ff29e74f71e1757
Nicolas Izard <tickrace.contact@gmail.com>
2025-07-19 19:16:56 +0200

    ✅ Corrige double export default dans vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['uuid'],
   },
-})
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173
-  },
-  build: {
-    outDir: 'dist'
-  }
-})
+});
