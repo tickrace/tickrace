2b015ac6d0347866bf7e05c21b2616bd8fc6759f
Nicolas Izard <tickrace.contact@gmail.com>
2025-07-19 19:09:15 +0200

    🔧 Ajout de uuid dans vite.config pour compatibilité build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['uuid'],
+  },
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
