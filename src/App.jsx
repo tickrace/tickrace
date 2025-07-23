1f13e6fb4037920a47afb29c9d90afb5267f9331
Nicolas Izard <tickrace.contact@gmail.com>
2025-07-23 11:29:13 +0200

    Forcer le re-render de Navbar sur changement de rôle avec key={currentRole}

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -14,34 +13,45 @@ import Signup from "./pages/Signup";
 import Admin from "./pages/Admin";
 import MonProfilCoureur from "./pages/MonProfilCoureur";
 import MonProfilOrganisateur from "./pages/MonProfilOrganisateur";
-import { UserProvider } from "./contexts/UserContext";
+import { UserProvider, useUser } from "./contexts/UserContext";
 import MonEspaceOrganisateur from "./pages/MonEspaceOrganisateur";
 import ListeInscriptions from "./pages/ListeInscriptions";
 import AuthRedirectWrapper from "./components/AuthRedirectWrapper";
+import { Toaster } from "react-hot-toast";
+
+function AppContent() {
+  const { currentRole } = useUser(); // 👈 récupération du rôle
+  return (
+    <>
+      <Navbar key={currentRole} /> {/* 👈 force le re-render */}
+      <Routes>
+        <Route path="/" element={<Courses />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/courses/:id" element={<CourseDetail />} />
+        <Route path="/organisateur/nouvelle-course" element={<NouvelleCourse />} />
+        <Route path="/organisateur/modifier-course/:id" element={<ModifierCourse />} />
+        <Route path="/formats" element={<ListeFormats />} />
+        <Route path="/inscription/:courseId" element={<InscriptionCourse />} />
+        <Route path="/coureur" element={<ProfilCoureur />} />
+        <Route path="/monprofilcoureur" element={<MonProfilCoureur />} />
+        <Route path="/monprofilorganisateur" element={<MonProfilOrganisateur />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/organisateur/mon-espace" element={<MonEspaceOrganisateur />} />
+        <Route path="/organisateur/inscriptions/:format_id" element={<ListeInscriptions />} />
+      </Routes>
+    </>
+  );
+}
 
 export default function App() {
   return (
     <UserProvider>
       <BrowserRouter>
         <AuthRedirectWrapper>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Courses />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/courses/:id" element={<CourseDetail />} />
-            <Route path="/organisateur/nouvelle-course" element={<NouvelleCourse />} />
-            <Route path="/organisateur/modifier-course/:id" element={<ModifierCourse />} />
-            <Route path="/formats" element={<ListeFormats />} />
-            <Route path="/inscription/:courseId" element={<InscriptionCourse />} />
-            <Route path="/coureur" element={<ProfilCoureur />} />
-            <Route path="/monprofilcoureur" element={<MonProfilCoureur />} />
-            <Route path="/monprofilorganisateur" element={<MonProfilOrganisateur />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/organisateur/mon-espace" element={<MonEspaceOrganisateur />} />
-            <Route path="/organisateur/inscriptions/:format_id" element={<ListeInscriptions />} />
-          </Routes>
+          <Toaster position="top-right" />
+          <AppContent />
         </AuthRedirectWrapper>
       </BrowserRouter>
     </UserProvider>
