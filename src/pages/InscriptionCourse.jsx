68f9bfc19e7b67d3287ad33406ba5c25741aece8
Nicolas Izard <tickrace.contact@gmail.com>
2025-07-28 13:24:16 +0200

    🐛 Correction typo double guillemets ligne 1 dans InscriptionCourse.jsx

--- src/pages/InscriptionCourse.jsx
+++ src/pages/InscriptionCourse.jsx
@@ -1,4 +1,4 @@
-""import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { supabase } from "../supabase";
 import PPSVerifier from "../components/PPSVerifier";
