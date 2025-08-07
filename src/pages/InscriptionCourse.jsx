0449662ee9cc11cbdb6dd2b5b6cf3cf64202cf8b
Nicolas Izard <tickrace.contact@gmail.com>
2025-08-07 16:52:55 +0200

    💳 Suppression des inscriptions groupées : adaptation bouton paiement individuel

--- src/pages/InscriptionCourse.jsx
+++ src/pages/InscriptionCourse.jsx
@@ -316,8 +316,7 @@ export default function InscriptionCourse() {
             </div>
           );
         })}
-
-    //    <button type="button" onClick={() => addInscription()} className="bg-blue-500 text-white px-4 py-2 rounded">+ Ajouter un coureur</button>
+      
         
         
 
