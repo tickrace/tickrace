38aa6128ecead3b3571cb0ee1ca058266cf1073f
Nicolas Izard <tickrace.contact@gmail.com>
2026-01-06 20:40:02 +0100

    chore: add api ping

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader("content-type", "application/json; charset=utf-8");
+  res.setHeader("cache-control", "no-store");
   res.status(200).send(JSON.stringify({ ok: true, ts: Date.now() }));
 }
